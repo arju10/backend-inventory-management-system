1cab221c6c9b42e99099de244589325cd7bc8963
arju10 <arjuarju71@gmail.com>
2024-03-31 02:53:47 +0600

    delete an employee

--- api/Employee/employee.controller.js
+++ api/Employee/employee.controller.js
@@ -3,11 +3,11 @@ const Employee = require("./employee.model");
 // Create a New Employee : "/api/v1/employees/add-employee"  >>>> Method [POST]
 exports.createEmployee = async (req, res) => {
     const employeeData = req.body;
-    try {  
-      const newEmployee = await Employee.create(employeeData);
-      res.status(201).json(newEmployee);
+    try {
+        const newEmployee = await Employee.create(employeeData);
+        res.status(201).json(newEmployee);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+        res.status(500).json({ message: error.message });
     }
 };
 
@@ -15,25 +15,44 @@ exports.createEmployee = async (req, res) => {
 
 exports.getAllEmployees = async (req, res) => {
     try {
-      const employees = await Employee.find();
-      res.status(200).json(employees);
+        const employees = await Employee.find();
+        res.status(200).json(employees);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+        res.status(500).json({ message: error.message });
     }
 };
 
-// Update a Employee by ID : "/api/v1/employees/update-employee/:id"  >>>> Method [GET]
+// Update a Employee by ID : "/api/v1/employees/update-employee/:id"  >>>> Method [PUT]
 
 exports.updateEmployee = async (req, res) => {
     const { id } = req.params;
     const updates = req.body;
     try {
-      const updatedEmployee = await Employee.findByIdAndUpdate(id, updates, { new: true });
-      if (!updatedEmployee) {
-        return res.status(404).json({ message: "Employee not found" });
-      }
-      res.status(200).json(updatedEmployee);
+        const updatedEmployee = await Employee.findByIdAndUpdate(id, updates, { new: true });
+        if (!updatedEmployee) {
+            return res.status(404).json({ message: "Employee not found" });
+        }
+        res.status(200).json(updatedEmployee);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+// Delete a Employee by ID : "/api/v1/employees/delete-employee/:id"  >>>> Method [DELETE]
+
+exports.deleteEmployee = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedEmployee = await Employee.findByIdAndDelete(id);
+        if (!deletedEmployee) {
+            return res.status(404).json({ message: "Employee not found" });
+        }
+        res.status(200).json({
+             success: true,
+             deletedEmployee 
+            });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 };
--- api/Employee/employee.route.js
+++ api/Employee/employee.route.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const Employee = require("./employee.model");
-const { createEmployee, getAllEmployees, updateEmployee } = require('./employee.controller');
+const { createEmployee, getAllEmployees, updateEmployee, deleteEmployee } = require('./employee.controller');
 
 // Here will be Employee Related API
 router.route('/add-employee').post(createEmployee);
 router.route('/allEmployees').get(getAllEmployees);
 router.route('/update-employee/:id').put(updateEmployee);
+router.route('/delete-employee/:id').delete(deleteEmployee);
 
 
 
